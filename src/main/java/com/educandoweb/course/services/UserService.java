c71f8f5864071f1cdaa07dc0320fad9a7f717e8a
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2022-09-22 15:44:35 -0300

    Service layer, component registration

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+//Indica que esta class é um componente de serviço do Spring, sendo assim possível fazer a injeção de depenencia com o "@Autowired" no controlador
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		//metodo "findById" retorna um "Optional"
+		Optional<User> obj = userRepository.findById(id);
+		
+		//metodo "get" pega o objeto do "Optional"
+		return obj.get();
+	}
+}
